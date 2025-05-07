7fcad7dc21f1c75cf24cbdb967d869988b0013d4
Nyandoya <nyandoyawere@gmail.com>
2025-05-07 08:53:28 +0300

    Week 7 Assignment Submission

--- answers.sql
+++ answers.sql
@@ -1,29 +1,42 @@
--- Quest 1 1NF
--- split the Products column into individual rows using SQL's STRING_SPLIT
-SELECT OrderID, CustomerName, TRIM(value) AS Product
-FROM ProductDetail
-CROSS APPLY STRING_SPLIT(Products, ', ');
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
--- Quest 2 2NF
--- split the OrderDetails table into two tables: Orders and OrderProducts
--- Step 1: Create the Orders table
+-- Question 2
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(255));
-
+    CustomerName VARCHAR(100)
+);
 INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
--- Step 2: Create the OrderProducts table
-CREATE TABLE OrderProducts (
+
+CREATE TABLE Product (
     OrderID INT,
-    Product VARCHAR(255),
+    Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
-INSERT INTO OrderProducts (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
